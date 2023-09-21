7da9e4ad7e6735e773be1b4949cbff2cfdd14970
Benjamin Herrick <NameinProgressTM@Bens-MacBook-Pro.local>
2023-09-21 14:48:06 -0500

    able to display the movies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movie_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +16,30 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+ app.get('/api/movies',async(req,res,next)=>{
+  try {
+    const SQL=`SELECT * FROM movies`
+    const response=await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+ })
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
-  `;
+    DROP TABLE IF EXISTS movies;
+    CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(30),
+      review INT 
+    );
+    INSERT INTO movies(name,review) VALUES('The Godfather',5);
+    INSERT INTO movies(name,review) VALUES('Star Wars', 4);
+    INSERT INTO movies(name,review) VALUES('The Room', 1);`;
+    await client.query(SQL);
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'; 
 
 const App = ()=> {
+  const [movies, setMovies]=useState([]);
+
+  useEffect(()=>{
+    const fetchMovies=async()=>{
+      const response=await axios.get('/api/movies');
+      console.log(response.data); 
+      setMovies(response.data);
+    };
+    fetchMovies();
+  },[]);
+
   return (
-    <h1>FullStack Template</h1>
+    <>
+      <h1>Movie Time</h1>
+      <h2>What should we watch?</h2>
+      <h2>We have ({movies.length}) of them</h2>
+      <ul>
+        {
+          movies.map((movie)=>{
+            return <li key={movie.id}>{movie.name}</li>
+          })
+        }
+      </ul>
+    </>
   );
 };
 
